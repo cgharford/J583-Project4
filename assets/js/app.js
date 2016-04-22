2aa5cbd4ffdb7a4c148ec828e1a7aaad3903ccbe
cgharford <charford@live.unc.edu>
2016-04-22 07:50:21 -0400

    Finished angular functionality and added new styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,54 @@ var victims = [];
 
 var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
 
-
 myApp.controller('MyController', [ '$scope', '$http', function($scope, $http) {
+  $("#personal-info").hide();
   $scope.currentPage = 1;
   $scope.victims = [];
+  $scope.showImage = false;
+  $scope.showLink = false;
 
   $http.get('../victims.json').success(function(data){
     $scope.victims = data;
   });
+
+  $scope.displayIndivudualInfo = function(person) {
+      $("#personal-info").show(500);
+      if (person.Photo != null && person.Photo != 0) {
+          $scope.showImage = true;
+          $scope.image = person.Photo;
+      }
+      else {
+          $scope.showImage = false;
+      }
+      console.log(person["News Story"]);
+      if (person["News Story"] != null && person["News Story"] != 0) {
+          $scope.showLink = true;
+          $scope.newsLink = person["News Story"];
+      }
+      else {
+          $scope.showLink = false;
+      }
+      if (person["City"] != null && person["City"] != 0) {
+          $scope.city = person["City"];
+      }
+      else {
+          $scope.city = "Location unknown";
+      }
+      $scope.name = person.Name;
+      $scope.cause = person['Cause of Death'];
+      $scope.date = person['Date of Death'];
+      $scope.gender = person['Gender'];
+      $scope.age = person['Age'];
+      $scope.race = person['Race'];
+      $scope.armed = person['Was the deceased armed?'];
+      $scope.priors = person['Did the deceased have priors?'];
+      $scope.officer = person['Was the officer involved fired or suspended?'];
+  }
+
+  $scope.hideIndivudualInfo = function() {
+      $("#personal-info").hide(500);
+  }
 }]);
 
 myApp.controller('OtherController', ['$scope', function($scope){}]);
